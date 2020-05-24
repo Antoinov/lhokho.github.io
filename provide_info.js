079cda67e0fef19f49e91eac8c0f1e2163df00b1
lhoumeau <maxime>
2020-05-24 12:44:20 +0200

    populate destination page

--- provide_info.js
+++ provide_info.js
@@ -1,35 +1,56 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDTzIHmzLtV18PlC_rhmUlVBoj0FMty2U8",
-    authDomain: "lhokho.firebaseapp.com",
-    databaseURL: "https://lhokho.firebaseio.com",
-    projectId: "lhokho",
-    storageBucket: "lhokho.appspot.com",
-    messagingSenderId: "914809444198",
-    appId: "1:914809444198:web:35ffb12ed619dd2de41aa7",
-    measurementId: "G-KEBX9V4BPX"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-//create firebase database reference
-var dbRef = firebase.database();
-
-console.log(dbRef)
-
 $(document).ready(function(){
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDTzIHmzLtV18PlC_rhmUlVBoj0FMty2U8",
+        authDomain: "lhokho.firebaseapp.com",
+        databaseURL: "https://lhokho.firebaseio.com",
+        projectId: "lhokho",
+        storageBucket: "lhokho.appspot.com",
+        messagingSenderId: "914809444198",
+        appId: "1:914809444198:web:35ffb12ed619dd2de41aa7",
+        measurementId: "G-KEBX9V4BPX"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    //create firebase database reference
+    var dbRef = firebase.database();
+
+    console.log(dbRef)
     var city_name = window.location.search.substr(1).split("=")[1];
-    $('#background').css({'background-image': 'img/lyon.jpg'});
+    city_name = "Paris";
+
+    $('#background').css({'background-image': 'url(img/city/'+city_name+'.jpg)'});
+
+    const capitalize = (s) => {
+      if (typeof s !== 'string') return ''
+      return s.charAt(0).toUpperCase() + s.slice(1)
+    }
 
 
     function readFirebase(city_name) {
 
-        firebase.database().child('destination').orderByChild('ville').equalTo(city_name).on("value", function(snapshot) {
-            console.log(snapshot.val());
-            snapshot.forEach(function(data) {
-                console.log(data.key);
-            });
+//        firebase.database().ref('destination').once('value').then(function(snapshot) {
+//          var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+//          console.log(snapshot.val())
+//        });
+        city_name = city_name.charAt(0).toUpperCase() + city_name.slice(1);
+        city_name = city_name.replace("_","-");
+
+
+        firebase.database().ref("city").orderByChild("ville").equalTo(city_name).once("value", function(snapshot) {
+            var data = snapshot.val()[0];
+
+            console.log('function provide info launched')
+            $( "#city_welcome" ).append(data['ville']);
+            $( "#city_region" ).append(data['region']);
+            $( "#city_departement" ).append(data['departement']);
+            $( "#city_population" ).append(data['population']);
+            $( "#city_densite" ).append(data['densite']);
+            $( "#city_gentile" ).append(data['gentile']);
+            $( "#city_altitude" ).append(data['altitude']);
+            $( "#city_superficie" ).append(data['superficie']);
+
         });
     }
 
