7d5024d143bb8e09da1f10015736d483a69ef2e2
M434796 <malhoumeau@airfrance.fr>
2020-06-04 14:37:51 +0200

    configure bar map

--- scripts/barMap.js
+++ scripts/barMap.js
@@ -7,24 +7,38 @@ function add_marker(name,position,icon_url,icon_size,price,layer){
     marker_destination.setIcon(custom_icon);
 
     var popup = L.popup({"maxWidth": "100%"});
-    var html = $('<a id="html_'+name+'" style="width: 100.0%; height: 100.0%;" href="http://www.google.com/search?q='+name+' bar" target="_blank""><br>'+name+'<br></a>')[0];
+    var html = $('<a id="html_'+name+'" style="width: 100.0%; height: 100.0%;" href="http://www.google.com/search?q='+name+' bar" target="_blank"">' +
+        '<br>'+name+'<br>' +
+        '</a>')[0];
 
     popup.setContent(html);
     marker_destination.bindPopup(popup);
     layer.addLayer(marker_destination);
-
 }
 
+function arrayRemove(arr, value) { return arr.filter(function(ele){ return ele != value; });}
+
 function update_map(markers,price){
-    console.log(price)
+    var clusterToClean = [];
     markers.eachLayer(function(layer){
-        console.log(layer)
-        if(layer.options.price > price){
-            layer.setOpacity(0);
-        }else{
-            layer.setOpacity(1)
+        if(layer.options.price !== undefined){
+            if(layer.options.price > price){
+                layer.setOpacity(0);
+                var visibleOne = markers.getVisibleParent(layer);
+                if(clusterToClean.indexOf(visibleOne) === -1){
+                    clusterToClean.push(visibleOne);
+                }
+            }else{
+                layer.setOpacity(1);
+                var anotherOne = markers.getVisibleParent(layer);
+                arrayRemove(clusterToClean,anotherOne);
+                anotherOne.setOpacity(1);
+            }
         }
     });
+    clusterToClean.forEach(function(cluster){
+        cluster.setOpacity(0);
+    })
 }
 
 $(document).ready(function(){
@@ -42,6 +56,8 @@ $(document).ready(function(){
         let bar_names = [];
         let bar_positions = [];
         var bar_HH_prices = [];
+        var bar_nHH_prices = [];
+        var bar_HH_hours = [];
         console.log(info_bars)
 
         info_station.on("value", function(dataset) {
@@ -59,12 +75,16 @@ $(document).ready(function(){
                     console.log('bar added');
                     bar_positions.push([node_data.latitude,node_data.longitude]);
                     bar_names.push(node_data.name);
+                    bar_HH_prices.push(Number(node_data["HHprice_1"]));
+                    bar_nHH_prices.push(Number(node_data["nHHprice_1"]));
+                    console.log(node_data["HHprice_1"]);
+                    let start = node_data["HH_start"];
+                    start = start.replace('[','').replace(']','').replace(',','h');
+                    let end = node_data["HH_end"];
+                    end = end.replace('[','').replace(']','').replace(',','h');
+                    bar_HH_hours.push([start,end]);
                 });
-                // to be replaced
 
-                for (var i=0, t=15; i<bar_names.length; i++) {
-                    bar_HH_prices.push(Math.round(Math.random() * t))
-                }
                 setupMap()
             });
 
@@ -106,7 +126,7 @@ $(document).ready(function(){
 
             for (var k = 0; k < station_names.length; k++) {
                 var name = station_names[k]
-                add_marker(name,station_positions[k],"images/icons/station.png",[40,40],0,route);
+                add_marker(name,station_positions[k],"images/icons/station.png",[40,40],undefined,route);
                 var circleCenter = station_positions[k];
 
                 var circleOptions = {
@@ -135,19 +155,24 @@ $(document).ready(function(){
 
             for (var j = 0; j < bar_names.length; j++) {
                 var name = bar_names[j]
-                add_marker(name,bar_positions[j],"images/icons/pint.png",[20,20],bar_HH_prices[j],markers)
+                if(isNaN(bar_nHH_prices[j])){
+                    add_marker(name,bar_positions[j],"images/icons/pint.png",[20,20],undefined,markers)
+                }
+                add_marker(name,bar_positions[j],"images/icons/pint.png",[20,20],bar_nHH_prices[j],markers)
             }
 
-
+            console.log(markers)
             slider = L.control.slider(function(value) {
                 update_map(markers,value)
             }, {
-                max: 15,
-                value: 15,
+                max: Math.round(Math.max(...bar_nHH_prices))+1,
+                value: Math.round(Math.max(...bar_nHH_prices)),
                 step:1,
                 size: '250px',
                 orientation:'vertical',
-                id: 'slider'
+                id: 'slider',
+                collapsed:false,
+                logo:'images/icons/mug.png'
             }).addTo(map);
 
             map.fitBounds(markers.getBounds());
