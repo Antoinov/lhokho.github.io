1d8e53750a9d34dabd23944af8a4de007c067353
M434796 <malhoumeau@airfrance.fr>
2020-06-11 21:31:11 +0200

    LK011 add modal to create a trip

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,15 +3,18 @@ $(document).ready(function() {
 
     function populate_menu(city_name,city_index) {
         //console.log(city_name);
-        let city_html = $('<li><a id="link_' + city_index + '" style="width: 100.0%; height: 100.0%;" href="destination?city=' + city_index + '" target="_blank""><br>' + city_name + '<br></a></li>')[0];
+        let city_html = $('<li><a id="link_' + city_index + '" style="width: 100.0%; height: 100.0%;" href="destination.html?city=' + city_index + '" target="_blank""><br>' + city_name + '<br></a></li>')[0];
         $("#destSubmenu").append(city_html);
+        $("#destination_select").append(new Option(city_name, city_index));
     }
 
     firebase.database().ref().child('city/station').once('value').then(function(datakey){
-        datakey.forEach(function(data,index){
+        count = 0;
+        datakey.forEach(function(data){
             city_name = data.val()[0].city;
-            //console.log(city_name)
-            populate_menu(city_name,index);
+            console.log(count)
+            populate_menu(city_name,count);
+            count = count + 1;
         });
     });
 
